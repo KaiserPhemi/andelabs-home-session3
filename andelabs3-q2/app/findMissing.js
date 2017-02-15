119e9b3afc187c6f8b294aaa87f95637019f7095
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-15 13:00:27 +0100

    find missing problem solved

--- andelabs3-q2/app/findMissing.js
+++ andelabs3-q2/app/findMissing.js
@@ -6,25 +6,26 @@
 			
 			/*Ternary operation to check input*/
 			var checkInput 	= (!(array1 instanceof Array) || !(array2 instanceof Array)) ? "Inputs must be an array":
+							  (array1.length === array2.length) ? 0 :
 							  (array1.length == 0 && array2.length == 0) ? 0 : findDifference(array1, array2);
 			return checkInput;
 
 			/*Function to compute difference*/
 			function findDifference(array1, array2){
+				
 				var filteredArr1 = array1.filter(function(element){
-					return array2.indexOf(element) == -1;}),
+					return array2.indexOf(element) == -1;});
 
-					filteredArr2 = array2.filter(function(element){
+				var	filteredArr2 = array2.filter(function(element){
 						return array1.indexOf(element) == -1;});
 
 				return parseInt((filteredArr1.concat(filteredArr2)), 10);
+					
+				}
 
 			
 			}
-		}
-
-
-	};
+		};
 	
 
 })();
