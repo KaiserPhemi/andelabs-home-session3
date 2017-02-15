cb907427bccd58986112f754da1bbf961ff1f3ce
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-15 12:41:06 +0100

    New files added and findMissing modified

--- andelabs3-q2/app/findMissing.js
+++ andelabs3-q2/app/findMissing.js
@@ -6,16 +6,18 @@
 			
 			/*Ternary operation to check input*/
 			var checkInput 	= (!(array1 instanceof Array) || !(array2 instanceof Array)) ? "Inputs must be an array":
-							  (array1.length == 0 && array2.length == 0) ? 0 : arrayFilter(array1, array2); 
-
+							  (array1.length == 0 && array2.length == 0) ? 0 : findDifference(array1, array2);
 			return checkInput;
 
-			/*Function that compares array inputs*/
-			function arrayFilter(array1, array2){
+			/*Function to compute difference*/
+			function findDifference(array1, array2){
 
+			
 			}
-
 		}
 
+
 	};
+	
+
 })();
