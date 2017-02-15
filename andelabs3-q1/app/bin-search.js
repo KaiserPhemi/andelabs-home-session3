fdbcf176a6b263229ad727ec6fac238d335e4f18
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-15 14:49:40 +0100

    Binary search solved with 7 failures

--- andelabs3-q1/app/bin-search.js
+++ andelabs3-q1/app/bin-search.js
@@ -44,5 +44,55 @@
 		return this;	
 	};
 
-	
+	/*Prototype to find element*/
+	Array.prototype.search = function(element){
+
+		this.count = 0;
+		this.index = -1;
+
+		var baseIndex = 0,
+			maxIndex = this.length-1,
+			currentIndex,
+			currentElement;
+
+		while(baseIndex <= maxIndex){
+				
+				currentIndex = Math.floor((baseIndex + maxIndex)/2);
+				currentElement = this[currentIndex];
+
+				/*Checks for index of search element*/
+				if (this[baseIndex] === element){
+					this.index = baseIndex;
+				}
+				else if(this[maxIndex] === element){
+					this.index = maxIndex;
+				}
+				else{
+					this.index = currentIndex;
+				}
+
+			
+				/*Checks either half of array*/
+				if (currentElement < element){
+					baseIndex = currentIndex + 1;
+				}
+				else if (currentElement > element){
+					maxIndex = currentIndex - 1;
+				}
+				
+				this.count++;
+		}
+
+		return new Detail(this.count, this.index, this.length);
+
+
+	};
+
+	/*Object to handle count, length and index*/
+	function Detail(count, index, length){
+		this.count = count;
+		this.index = index;
+		this.length = length;
+	}
+
 })();
