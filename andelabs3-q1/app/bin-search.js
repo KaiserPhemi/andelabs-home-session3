ff8d29b6f5da23d732c233640faecba25e4bd8d6
Kaiser Phemi <kaiser.phemi@protonmail.ch>
2017-02-16 15:41:11 +0100

    Binary search solved but with 1 failure

--- andelabs3-q1/app/bin-search.js
+++ andelabs3-q1/app/bin-search.js
@@ -5,61 +5,21 @@
 	module.exports = [].prototype;
 
 
-	/*Array of 1 to 20*/
-	Array.prototype.toTwenty = function(){
-
-		var counter = 1;
-
-		while(counter <= 20){
-
-			this.push(counter);
-			counter++;
-		}
-		return this;
-	};
-
-	/*Array for multiples of 2 to 40*/
-	Array.prototype.toForty = function(){
-
-		var counter = 2;
-
-		while(counter <= 40){
-
-			this.push(counter);
-			counter += 2;
-		}
-		return this;
-	};
-
-	/*Array for multiples of 10 to 1000*/
-	Array.prototype.toOneThousand = function(){
-
-		var counter = 10;
-
-		while(counter <= 1000){
-
-			this.push(counter);
-			counter += 10;
-		}
-		return this;	
-	};
-
 	/*Prototype to find element*/
 	Array.prototype.search = function(element){
-
+				
 		this.count = 0;
 		this.index = -1;
 
 		var baseIndex = 0,
-			maxIndex = this.length-1,
-			currentIndex,
-			currentElement;
-
+			maxIndex = this.length-1;
+	
 		while(baseIndex <= maxIndex){
 
 				/*Splits array in half*/				
-				currentIndex = Math.floor((baseIndex + maxIndex)/2); 
-				currentElement = this[currentIndex];
+				var currentIndex = Math.floor((baseIndex + maxIndex)/2); 
+				var currentElement = this[currentIndex];
+
 
 				/*Checks for index of search element*/
 				if (this[baseIndex] === element){
@@ -72,25 +32,26 @@
 					this.index = maxIndex;
 					break;
 				}
-				else{
-					
-					this.index = currentIndex;
-					break;
-				}
 
-			
 				/*Checks either half of array*/
 				if (currentElement < element){
 					
-					aseIndex = currentIndex + 1;
-					break;
+					baseIndex = currentIndex + 1;
 				}
 				else if (currentElement > element){
 					
 					maxIndex = currentIndex - 1;
+				}
+				else if(currentElement === element){
+					
+					this.index = currentIndex;
 					break;
 				}
-				
+				else{
+					this.index = currentIndex;
+					break;
+				}
+								
 				this.count++;
 		}
 
@@ -99,6 +60,44 @@
 
 	};
 
+
+	/*Array of 1 to 20*/
+	Array.prototype.toTwenty = function(){
+
+		var counter = 1;
+		while(counter <= 20){
+
+			this.push(counter);
+			counter ++;
+		}
+		return this;
+	};
+
+	/*Array for multiples of 2 to 40*/
+	Array.prototype.toForty = function(){
+	
+		var counter = 2;
+		while(counter <= 40){
+
+			this.push(counter);
+			counter += 2;
+		}
+		return this;
+	};
+
+	/*Array for multiples of 10 to 1000*/
+	Array.prototype.toOneThousand = function(){
+
+		var counter = 10;
+		while(counter <= 1000){
+
+			this.push(counter);
+			counter += 10;
+		}
+		return this;	
+	};
+
+		
 	/*Object to handle count, length and index*/
 	function Detail(count, index, length){
 		this.count = count;
